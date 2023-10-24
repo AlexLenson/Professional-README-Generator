74cb3e5b1a778a70bb54d40526b5c4c64423d047
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-10-24 00:08:26 -0400

    created README test

--- index.js
+++ index.js
@@ -1,13 +1,81 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateREADME = ({ title, location, github, linkedin }) =>
+`# ${title}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+## Description
+${description}
 
-// TODO: Create a function to initialize app
-function init() {}
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+- [Video Demonstration](#video-demonstration)
 
-// Function call to initialize app
-init();
+## Installation
+${install}
+
+## Usage
+${usage}
+
+## Contributing
+${contribution}
+
+## Tests
+${test}
+
+## License
+Project is covered under the ${license} license
+
+## Questions
+Direct any quetsions to ${username} at ${profile}
+For additional questions, email me at ${email}
+
+## Video Demonstration
+See a video demonstration: [Watch Video](https://drive.google.com/your_shareable_link_here)`;
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'location',
+            message: 'Where are you from?',
+        },
+        {
+            type: 'input',
+            name: 'hobby',
+            message: 'What is your favorite hobby?',
+        },
+        {
+            type: 'input',
+            name: 'food',
+            message: 'What is your favorite food?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username',
+        },
+        {
+            type: 'input',
+            name: 'linkedin',
+            message: 'Enter your LinkedIn URL.',
+        },
+    ])
+    .then((answers) => {
+        const htmlPageContent = generateHTML(answers);
+
+        fs.writeFile('index.html', htmlPageContent, (err) =>
+            err ? console.log(err) : console.log('Successfully created index.html!')
+        );
+    });
